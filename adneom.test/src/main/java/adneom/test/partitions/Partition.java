ec84fa2697a1f1939f24010a440ac8ec59bd7ecd
ghostp <ghostp@ghostp-HP-Pavilion-dv7-Notebook-PC>
2016-03-11 17:40:15 +0100

    Test Junit 1

--- adneom.test/src/main/java/adneom/test/partitions/Partition.java
+++ adneom.test/src/main/java/adneom/test/partitions/Partition.java
@@ -1,4 +1,11 @@
 /**
+ * Partition - Manipulation de liste d'entier
+ * 
+ * @version 1.0
+ * 
+ * @author  Aminou KADRY
+ * @copyright Aminou KADRY
+ * @date 11/03/2016
  * 
  */
 
@@ -13,6 +20,14 @@ import org.apache.commons.collections4.ListUtils;
 
 public class Partition {
 
+	/**
+	 * doPartition
+	 * 
+	 * @param largeList
+	 * @param targetSize
+	 * @return
+	 */
+	
 
 	public List<List<Integer>> doPartition(List largeList, Integer targetSize) {
 
--- adneom.test/src/main/test/PartitionTest.java
+++ adneom.test/src/main/test/PartitionTest.java
@@ -2,13 +2,31 @@ package adneom.test.partitions;
 
 import static org.junit.Assert.*;
 
+import java.awt.List;
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class PartitionTest {
+	
+	Partition list = null;
+	
+	@Before
+	public void setUp() throws Exception {
+		list = new Partition();
+	}
 
-	@Test
+	@Test(expected=IllegalArgumentException.class)
 	public void test() {
-		fail("Not yet implemented");
+		
+		List largeList= new ArrayList();
+		
+		for(int i =0;i<10;i++)
+			largeList.add(i);
+		list.doPartition(largeList, -2)
+		
+		
 	}
 
 }
