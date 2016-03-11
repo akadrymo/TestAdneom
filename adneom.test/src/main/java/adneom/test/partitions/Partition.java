ff5cff0dd8dd452df15b77f1fffcc7fc3f2b72d8
ghostp <ghostp@ghostp-HP-Pavilion-dv7-Notebook-PC>
2016-03-11 16:49:31 +0100

    Creation du Test Junit pour la classe Partition

--- adneom.test/src/main/java/adneom/test/partitions/Partition.java
+++ adneom.test/src/main/java/adneom/test/partitions/Partition.java
@@ -13,20 +13,8 @@ import org.apache.commons.collections4.ListUtils;
 
 public class Partition {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		List list = new ArrayList<Integer>();
-		
-		for (int i=0;i<10;i++)
-			list.add(i);
-		
-		List<List<Integer>> output = doPartition(list, 1);
-		System.out.println(output);
 
-	}
-
-	public static List<List<Integer>> doPartition(List largeList, Integer targetSize) {
+	public List<List<Integer>> doPartition(List largeList, Integer targetSize) {
 
 		List<List<Integer>> output = ListUtils.partition(largeList, targetSize);
 		return output;
---
+++ adneom.test/src/main/test/PartitionTest.java
@@ -0,0 +1,14 @@
+package adneom.test.partitions;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PartitionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
